063ec3bdb4a92c6b142f575d977578664b9f6e1e
yeemaae <noobproger@gmail.com>
2024-04-15 21:52:20 +0500

    Added Product Models

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
 ]
+
+
--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 
 def index(request):
-    context = {
-        'title': 'Store',
-    }
+    context = {'title': 'Store', }
     return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
